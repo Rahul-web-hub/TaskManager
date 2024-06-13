ac992948dad3a886107eca841a38c8d64f55a10d
buky <buky6180@gmail.com>
2024-06-13 16:53:25 +0530

    message

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY =env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -128,7 +128,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
